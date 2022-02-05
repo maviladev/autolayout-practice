078941790593e5f23ca5ddf62a421b393377ce4f
Marcos Avila <marcosa31@gmail.com>
2022-02-04 22:30:00 -0600

    Example of using layout guides for spacing controls

---
+++ LayoutMarginAsSpacerGuides/LayoutMarginAsSpacerGuides/ViewController.swift
@@ -0,0 +1,87 @@
+//
+//  ViewController.swift
+//  LayoutMarginAsSpacerGuides
+//
+//  Created by Marcos Avila Rodriguez on 04/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        setupViews()
+    }
+
+    func setupViews()  {
+        //Controls
+        let leadingGuide = UILayoutGuide()
+        let okButton = makeButton(text: "OK", color: UIColor.darkBlue)
+        let middleGuide = UILayoutGuide()
+        let cancelButton = makeButton(text: "Cancel", color: UIColor.darkGreen)
+        let trailingGuide = UILayoutGuide()
+        
+        //Add controls to view
+        view.addSubview(okButton)
+        view.addSubview(cancelButton)
+        //Add layout guide
+        view.addLayoutGuide(leadingGuide)
+        view.addLayoutGuide(middleGuide)
+        view.addLayoutGuide(trailingGuide)
+        
+        //setup constraints
+        let margins = view.layoutMarginsGuide
+        
+        
+        NSLayoutConstraint.activate([
+            //leading guide
+            margins.leadingAnchor.constraint(equalTo: leadingGuide.leadingAnchor),
+            leadingGuide.trailingAnchor.constraint(equalTo: okButton.leadingAnchor),
+            
+            //middle guide
+            okButton.trailingAnchor.constraint(equalTo: middleGuide.leadingAnchor),
+            middleGuide.trailingAnchor.constraint(equalTo: cancelButton.leadingAnchor),
+            
+            //button equal widths
+            okButton.widthAnchor.constraint(equalTo: cancelButton.widthAnchor),
+            
+            //spacer equal widths
+            leadingGuide.widthAnchor.constraint(equalTo: middleGuide.widthAnchor),
+            leadingGuide.widthAnchor.constraint(equalTo: trailingGuide.widthAnchor),
+            
+            //vertical position
+            leadingGuide.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            middleGuide.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            trailingGuide.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            okButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            cancelButton.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            
+        ])
+        
+    }
+    
+    func makeButton(text: String, color: UIColor) -> UIButton {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle(title, for: .normal)
+        button.titleLabel?.adjustsFontSizeToFitWidth = true
+        button.contentEdgeInsets = UIEdgeInsets.init(top: 8, left: 16, bottom: 8, right: 16)
+        button.backgroundColor = color
+        
+        return button
+    }
+    
+}
+
+extension UIColor {
+    static let darkBlue = UIColor(red: 10/255, green: 132/255, blue: 255/255, alpha: 1)
+    static let darkGreen = UIColor(red: 48/255, green: 209/255, blue: 88/255, alpha: 1)
+    static let darkOrange = UIColor(red: 255/255, green: 149/255, blue: 0/255, alpha: 1)
+    static let darkRed = UIColor(red: 255/255, green: 59/255, blue: 48/255, alpha: 1)
+    static let darkTeal = UIColor(red: 90/255, green: 200/255, blue: 250/255, alpha: 1)
+    static let darkYellow = UIColor(red: 255/255, green: 204/255, blue: 0/255, alpha: 1)
+}
+
